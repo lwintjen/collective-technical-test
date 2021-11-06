0c1bc0e514ac712c7934b1b6594bba8da39b975f
Loris Wintjens <loris.wintjens@gmail.com>
2021-11-06 16:13:41 +0100

    feature: add tests

--- server/test/search.test.js
+++ server/test/search.test.js
@@ -14,3 +14,22 @@ describe('Fetch 150 crypto currencies', () => {
     })
     });
 });
+
+describe('Testing search endpoint', () => {
+ it('it should return bitcoin crypto currency', () => {
+    chai.request(server).get('/api/search?name=bitcoin').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.equal(1);
+        res.body[0].name.should.be.equal("Bitcoin");
+    })
+    });
+
+     it('it should not find anything', () => {
+    chai.request(server).get('/api/search?name=loris').end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.equal(0);
+    })
+    });
+});
