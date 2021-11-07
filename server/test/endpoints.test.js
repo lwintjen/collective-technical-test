819cf0afc2e3391a246588c5b85705b49580bd98
Loris Wintjens <loris.wintjens@gmail.com>
2021-11-08 00:08:17 +0100

    fix: test

--- server/test/endpoints.test.js
+++ server/test/endpoints.test.js
@@ -1,4 +1,4 @@
-const server = "http://localhost:8080"
+const server = "https://api-collective.herokuapp.com/api"
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 
 describe('Fetch 150 crypto currencies', () => {
  it('it should return 150 crypto currencies', () => {
-    chai.request(server).get('/api/fetch-cryptos').end((err, res) => {
+    chai.request(server).get('/fetch-cryptos').end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
         res.body.length.should.be.equal(150);
@@ -17,16 +17,16 @@ describe('Fetch 150 crypto currencies', () => {
 
 describe('Testing search endpoint', () => {
  it('it should return bitcoin crypto currency', () => {
-    chai.request(server).get('/api/search?name=bitcoin').end((err, res) => {
+    chai.request(server).get('/search?name=bitcoin').end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
-        res.body.length.should.be.equal(1);
+        res.body.length.should.be.above(1);
         res.body[0].name.should.be.equal("Bitcoin");
     })
     });
 
      it('it should not find anything', () => {
-    chai.request(server).get('/api/search?name=loris').end((err, res) => {
+    chai.request(server).get('/search?name=loris').end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('array');
         res.body.length.should.be.equal(0);
