7b97ee2a0d0191ba855ea3d3b68b751612d87e87
Loris Wintjens <loris.wintjens@gmail.com>
2021-11-08 00:44:59 +0100

    remove useless types file

--- collective-web-app/src/components/Tabs.tsx
+++ collective-web-app/src/components/Tabs.tsx
@@ -1,68 +1,74 @@
-import { useState, SyntheticEvent } from "react";
+import { useState, SyntheticEvent, ReactNode } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import CoinsList from "./CoinsList";
-import { TabPanelProps } from "../types/tabpanel";
 import { CoinCapURIResponse } from "../types/currency";
 
 interface Props {
-  coins: CoinCapURIResponse[];
-  setCoins: (coins: CoinCapURIResponse[]) => void;
+    coins: CoinCapURIResponse[];
+    setCoins: (coins: CoinCapURIResponse[]) => void;
 }
 
-const TabPanel = (props: TabPanelProps) => {
-  const { children, value, index, ...other } = props;
+interface TabPanelProps {
+    children?: ReactNode;
+    index: number;
+    value: number;
+}
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      aria-labelledby={`tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography component={"span"}>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+
+const TabPanel = (props: TabPanelProps) => {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`tabpanel-${index}`}
+            aria-labelledby={`tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography component={"span"}>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 };
 
 const a11yProps = (index: number) => {
-  return {
-    id: `tab-${index}`,
-    "aria-controls": `tabpanel-${index}`,
-  };
+    return {
+        id: `tab-${index}`,
+        "aria-controls": `tabpanel-${index}`,
+    };
 };
 
 const CoinsTabs: React.FC<Props> = (props) => {
-  const { coins, setCoins } = props;
-  const [value, setValue] = useState(0);
+    const { coins, setCoins } = props;
+    const [value, setValue] = useState(0);
 
-  const handleChange = (event: SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+    const handleChange = (event: SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs value={value} onChange={handleChange} aria-label="coins tab">
-          <Tab label="List" {...a11yProps(0)} />
-          <Tab label="My watchlist" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <CoinsList coins={coins} displayAllItems={true} setCoins={setCoins} />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <CoinsList coins={coins} displayAllItems={false} setCoins={setCoins} />
-      </TabPanel>
-    </Box>
-  );
+    return (
+        <Box sx={{ width: "100%" }}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs value={value} onChange={handleChange} aria-label="coins tab">
+                    <Tab label="List" {...a11yProps(0)} />
+                    <Tab label="My watchlist" {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <TabPanel value={value} index={0}>
+                <CoinsList coins={coins} displayAllItems={true} setCoins={setCoins} />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <CoinsList coins={coins} displayAllItems={false} setCoins={setCoins} />
+            </TabPanel>
+        </Box>
+    );
 };
 
 export default CoinsTabs;
