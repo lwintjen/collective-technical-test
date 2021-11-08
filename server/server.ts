fdeb36add90eb6c61c7dd3ff1a8c38054d779a7b
Loris Wintjens <loris.wintjens@gmail.com>
2021-11-08 10:33:50 +0100

    fix: cors issue

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ const cfg = initConfig();
 const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://sharp-snyder-625092.netlify.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://sharp-snyder-625092.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
